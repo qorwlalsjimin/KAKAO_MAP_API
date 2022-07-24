948bc4bb2ffc9ef2f3698a3e9e5d8676af34fa5d
지민 백 <qorwlalsjimin@naver.com>
2022-07-25 08:05:59 +0900

    build.gradle에 dependencies 내용 추가

--- app/src/main/java/kr/hs/s2104/map_api/MainActivity.java
+++ app/src/main/java/kr/hs/s2104/map_api/MainActivity.java
@@ -3,6 +3,9 @@ package kr.hs.s2104.map_api;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+//
+//        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+//        mapViewContainer.addView(mapView);
     }
+
 }
